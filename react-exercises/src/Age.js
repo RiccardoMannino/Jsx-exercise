c4f4dfa11ee83fa656eabee8f7745f5c1a2424b5
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-08-25 23:08:02 +0200

    fix conditional-rendering-01

--- react-exercises/src/Age.js
+++ react-exercises/src/Age.js
@@ -2,6 +2,10 @@ import React from "react";
 
 export class Age extends React.Component {
   render() {
-    return <>{this.props.age > 18 && <p>Your Age is, {this.props.age}</p>}</>;
+    return (
+      <>
+        <p>Your Age is, {this.props.age}</p>
+      </>
+    );
   }
 } // Se il valore della props age è maggiore di 18 allora verrà renderizzato il componente Age altrimenti non sarà renderizzato
