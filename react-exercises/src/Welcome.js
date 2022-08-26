7d1148290ffb96a968140cfb3cdd117e28912cf4
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-08-26 15:25:07 +0200

    fix props-05

--- react-exercises/src/Welcome.js
+++ react-exercises/src/Welcome.js
@@ -8,9 +8,7 @@ export class Welcome extends React.Component {
         <p>
           Welcome, <strong>{this.props.name}</strong>
         </p>
-        <p>
-          <Age age={this.props.age} />
-        </p>
+        <Age age={this.props.age} />
       </>
     );
   }
