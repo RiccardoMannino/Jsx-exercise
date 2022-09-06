08850ea5b4f86590c9697cb93072111eb76fa5b1
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-09-06 13:42:44 +0200

    lists-01

--- react-exercises/src/Colors.js
+++ react-exercises/src/Colors.js
@@ -2,12 +2,13 @@ import React from "react";
 
 export class Colors extends React.Component {
   render() {
-    //
-
-    const colori = this.props.items.map((colore) => <li>{colore}</li>);
     return (
       <div>
-        <ul>{colori}</ul>
+        <ul>
+          {this.props.items.map((colore) => (
+            <li>{colore}</li>
+          ))}
+        </ul>
       </div>
     );
   }
