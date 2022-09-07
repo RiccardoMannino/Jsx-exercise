51e2b6ba853dae9c49bee5fed7d03c015e730e91
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-09-08 01:34:48 +0200

    styling-components-04

--- react-exercises/src/ToDolist.js
+++ react-exercises/src/ToDolist.js
@@ -19,8 +19,8 @@ export class TodoList extends React.Component {
     const name = event.target.name;
     const todo = this._inputRef.current.value;
 
-    this.setState((state) => ({
-      id: (state.id = [""]),
+    this.setState(() => ({
+      [name]: this.state.id.pop(),
     }));
   };
 
@@ -36,18 +36,30 @@ export class TodoList extends React.Component {
   render() {
     return (
       <>
-        <ul>
+        <ul className="bg-rose-800 w-1/6 ml-2 border-2 rounded-sm">
           {this.state.id.map((todos, index) => (
             <li key={todos + index}>
               {todos}
-              <button onClick={this.handleReset}>Rimuovi Elemento</button>
+              <button className="ml-2 " onClick={this.handleReset}>
+                Rimuovi Elemento
+              </button>
             </li>
           ))}
         </ul>
 
-        <input ref={this._inputRef}></input>
-        <button onClick={this.handleTodoList}>Aggiungi Task</button>
-        <button onClick={this.handleReset2}>Rimuovi Task</button>
+        <input
+          className="border-t-indigo-600 bg-lime-400 p-3 ml-2 mr-2 mt-2"
+          ref={this._inputRef}
+        ></input>
+        <button
+          className="bg-sky-500 p-3 mr-2 mt-2"
+          onClick={this.handleTodoList}
+        >
+          Aggiungi Task
+        </button>
+        <button className="bg-cyan-900 p-3  " onClick={this.handleReset2}>
+          Rimuovi Task
+        </button>
       </>
     );
   }
