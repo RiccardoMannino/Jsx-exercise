38ab707431c1926c363cee477039339a73cd1150
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-09-10 14:05:03 +0200

    render-props-01

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -29,7 +29,56 @@ export class App extends React.Component {
         <Login />
         <UncontrolledLogin />
         <Colors items={["Rosso", "Verde", "Giallo", "Blu"]} />
-        <TodoList />
+        <TodoList
+          render={({
+            id,
+            _inputRef,
+            handleReset,
+            handleReset2,
+            handleTodoList,
+          }) => {
+            return (
+              <>
+                <ul className=" flex-col p-2  bg-slate-600 w-fit ml-2 rounded">
+                  {id.map((todos, index) => (
+                    <>
+                      <li
+                        className="  bg-red-600 text-zinc-100 w-fit mr-4 mb-2 rounded flex justify-between"
+                        key={todos + index}
+                      >
+                        {todos}
+                      </li>
+                      <button
+                        className=" bg-lime-500 min-w-fit ml-5 rounded mb-2
+                 "
+                        onClick={handleReset}
+                      >
+                        Rimuovi Elemento
+                      </button>
+                    </>
+                  ))}
+                </ul>
+
+                <input
+                  className="border-t-indigo-600 bg-lime-400 p-3 ml-2 mr-2 mt-2"
+                  ref={_inputRef}
+                ></input>
+                <button
+                  className="bg-sky-500 p-3 mr-2 mt-2 rounded"
+                  onClick={handleTodoList}
+                >
+                  Aggiungi Task
+                </button>
+                <button
+                  className="bg-cyan-900 p-3 rounded "
+                  onClick={handleReset2}
+                >
+                  Rimuovi Task
+                </button>
+              </>
+            );
+          }}
+        ></TodoList>
       </Container>
     );
   }
