8ad3edb0a9baed490e91e441e9d8bdb138fa38af
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-09-16 00:47:34 +0200

    custom-hooks-03

--- react-exercises/src/GithubUser.js
+++ react-exercises/src/GithubUser.js
@@ -1,36 +1,14 @@
-import { useState, useEffect } from "react";
+import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser({ username }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false); // setta lo stato di caricamento
-  const [error, setError] = useState(null); // setta lo stato per gli errori
-
-  useEffect(() => {
-    const fetchDati = async (username) => {
-      setLoading(true); // caricamento
-      setError(null); // nessun errore
-      try {
-        const response = await fetch(
-          `https://api.github.com/users/${username}`
-        );
-        const json = await response.json();
-        setData(json);
-      } catch (error) {
-        setError(error); // errore
-        setData(null); // quindi nessun dato
-      } finally {
-        setLoading(false); // rimozione della scritta di caricamento
-      }
-    };
-    fetchDati(username);
-  }, [username]);
+  const { data, error, loading } = useGithubUser(username);
 
   return (
     <div>
       {/* caricamento */}
-      {loading && <h1>Loading...</h1>}
+      {/* {loading && <h1>Loading...</h1>} */}
       {/* eventuali errori */}
-      {error && <h1>There has been an error</h1>}
+      {/* {error && <h1>There has been an error</h1>} */}
       {/* risposta */}
       {data && <h1>{data.name}</h1>}
     </div>
