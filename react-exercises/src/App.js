e52f837bdae5bfdab0db11315a6744842d31d009
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-09-15 11:01:26 +0200

    useEffect-04

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -12,6 +12,7 @@ import { LanguageContext } from "./LanguageContext";
 import { DisplayLanguage } from "./DisplayLanguage";
 import { Sum } from "./Sum";
 import { GithubUser } from "./GithubUser";
+import { GithubUserList } from "./GithubUserList ";
 
 export function App() {
   // state = {
@@ -41,7 +42,7 @@ export function App() {
       {/* <Message /> */}
       {/* <Welcome name="Riccardo" age={18} /> */}
       {/* <Counter timeout={1000} incrementBy={1} initialValue={0} /> */}{" "}
-      <GithubUser username="RiccardoMannino" />
+      <GithubUserList />
       <ClickCounter initialValue={0} />
       <ToDoList />
       <Counter />
