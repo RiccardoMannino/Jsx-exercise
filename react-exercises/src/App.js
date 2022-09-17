e7dc716e8a9343b260ec46ae3cb963ffa766528c
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-09-18 00:56:01 +0200

    react-router-02

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
+import { ClickCounter } from "./ClickCounter";
 
 export function App() {
-  const [language, setLanguage] = useState("en");
-  function handleLanguageChange(event) {
-    setLanguage(event.target.value);
-  }
+  // const [language, setLanguage] = useState("en");
+  // function handleLanguageChange(event) {
+  //   setLanguage(event.target.value);
+  // }
 
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Welcome name="Riccardo" />}></Route>
+        <Route path="/" element={<Welcome name="Riccardo" />} />
+        <Route path="counter" element={<ClickCounter />} />
       </Routes>
     </div>
   );
