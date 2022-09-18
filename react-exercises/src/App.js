8e2420307740cdddf50ef89353b79fc6721d1538
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-09-18 16:50:37 +0200

    react-router-04

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { ClickCounter } from "./ClickCounter";
 import { ShowGithubUser } from "./ShowGithubUser";
@@ -7,6 +7,20 @@ import { ShowGithubUser } from "./ShowGithubUser";
 export function App() {
   return (
     <div>
+      <div>
+        <h1>My awesome app!</h1>
+        <div>
+          <Link className="mr-3" to="/">
+            Home
+          </Link>
+
+          <Link className="mr-3" to="counter">
+            ClickCounter
+          </Link>
+
+          <Link to="users/:username">ShowGithubUser</Link>
+        </div>
+      </div>
       <Routes>
         <Route path="/" element={<Welcome name="Riccardo" />} />
         <Route path="counter" element={<ClickCounter initialValue={0} />} />
