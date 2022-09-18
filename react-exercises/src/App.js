83b01563388d599e0ea8ec1ff3cb6e613e249011
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-09-18 16:30:56 +0200

    react-router-03

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -2,18 +2,15 @@ import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { ClickCounter } from "./ClickCounter";
+import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
-  // const [language, setLanguage] = useState("en");
-  // function handleLanguageChange(event) {
-  //   setLanguage(event.target.value);
-  // }
-
   return (
     <div>
       <Routes>
         <Route path="/" element={<Welcome name="Riccardo" />} />
         <Route path="counter" element={<ClickCounter initialValue={0} />} />
+        <Route path="users/:username" element={<ShowGithubUser />} />
       </Routes>
     </div>
   );
