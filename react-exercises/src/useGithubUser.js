06faff0e9788a0d57283cb909310a6a52086ec66
Riccardo Mannino <80473943+RiccardoMannino@users.noreply.github.com>
2022-09-23 15:05:37 +0200

    SWR-04

--- react-exercises/src/useGithubUser.js
+++ react-exercises/src/useGithubUser.js
@@ -1,11 +1,8 @@
 import useSWR, { mutate } from "swr";
 
-const fetcher = (url) => fetch(url).then((response) => response.json()); // questa funzione viene passata come secpndo parametro del SWR
-
 export function useGithubUser(username) {
-  const { data, error } = useSWR(
-    () => (username ? `https://api.github.com/users/${username}` : null),
-    fetcher
+  const { data, error } = useSWR(() =>
+    username ? `https://api.github.com/users/${username}` : null
   );
 
   function handleRefresh() {
